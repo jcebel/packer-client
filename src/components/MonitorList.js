4f8774984bf64c99e1b874108c945ba082ebc7ed
Lukas <lukas.vorwerk@tum.de>
2019-06-12 22:07:34 +0200

    initial commit create empty components 'MonitorList' and 'MonitorListItem'

---
+++ src/components/MonitorList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MonitorList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
