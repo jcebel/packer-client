f84a545f9e10bcbb436a207765d4d31c5c70bef6
Lukas <lukas.vorwerk@tum.de>
2019-06-13 16:14:52 +0200

    - add header for each list item with additional information (date, price, delivery no) - delete comments - add additional list items for demonstration purposes -> still static

--- src/components/MonitorList.js
+++ src/components/MonitorList.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 
 import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
-import CardGroup from "react-bootstrap/CardGroup";
 import Badge from "react-bootstrap/Badge";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
@@ -13,76 +11,15 @@ class MonitorList extends Component {
     render() {
         return (
             <Container>
-                {/*<Card>*/}
-                {/*    <Card.Header as="h5">Chair</Card.Header>*/}
-                {/*    <Card.Body>*/}
-                {/*        <Card.Title>Special title treatment</Card.Title>*/}
-                {/*        <Card.Text>*/}
-                {/*            With supporting text below as a natural lead-in to additional content.*/}
-                {/*        </Card.Text>*/}
-                {/*        <Button variant="primary">Go somewhere</Button>*/}
-                {/*    </Card.Body>*/}
-                {/*</Card>*/}
-                {/*<Card>*/}
-                {/*    <Card.Header as="h5">Dish Set</Card.Header>*/}
-                {/*    <Card.Body>*/}
-                {/*        <Card.Title>Special title treatment</Card.Title>*/}
-                {/*        <Card.Text>*/}
-                {/*            With supporting text below as a natural lead-in to additional content.*/}
-                {/*        </Card.Text>*/}
-                {/*        <Button variant="primary">Go somewhere</Button>*/}
-                {/*    </Card.Body>*/}
-                {/*</Card>*/}
-                {/*<CardGroup>*/}
-                {/*    <Card>*/}
-                {/*        <Card.Header as="h5">Chair</Card.Header>*/}
-                {/*        <Card.Body>*/}
-                {/*            <Card.Title>Chair</Card.Title>*/}
-                {/*            <Card.Text>*/}
-                {/*                This is a wider card with supporting text below as a natural lead-in to*/}
-                {/*                additional content. This content is a little bit longer.*/}
-                {/*            </Card.Text>*/}
-                {/*        </Card.Body>*/}
-                {/*        <Card.Footer>*/}
-                {/*            <small className="text-muted">Last updated 3 mins ago</small>*/}
-                {/*        </Card.Footer>*/}
-                {/*    </Card>*/}
-                {/*    <Card>*/}
-                {/*        <Card.Header as="h5">"" </Card.Header>*/}
-                {/*        <Card.Body>*/}
-                {/*            <Card.Title>Card title</Card.Title>*/}
-                {/*            <Card.Text>*/}
-                {/*                This card has supporting text below as a natural lead-in to additional*/}
-                {/*                content.{' '}*/}
-                {/*            </Card.Text>*/}
-                {/*        </Card.Body>*/}
-                {/*        <Card.Footer>*/}
-                {/*            <small className="text-muted">Last updated 3 mins ago</small>*/}
-                {/*        </Card.Footer>*/}
-                {/*    </Card>*/}
-                {/*    <Card>*/}
-                {/*        <Card.Img variant="top" src="holder.js/100px160" />*/}
-                {/*        <Card.Body>*/}
-                {/*            <Card.Title>Card title</Card.Title>*/}
-                {/*            <Card.Text>*/}
-                {/*                This is a wider card with supporting text below as a natural lead-in to*/}
-                {/*                additional content. This card has even longer content than the first to*/}
-                {/*                show that equal height action.*/}
-                {/*            </Card.Text>*/}
-                {/*        </Card.Body>*/}
-                {/*        <Card.Footer>*/}
-                {/*            <small className="text-muted">Last updated 3 mins ago</small>*/}
-                {/*        </Card.Footer>*/}
-                {/*    </Card>*/}
-                {/*</CardGroup>*/}
-
-                <div className="list-group">
+                <p/>
+                <div className="list-group">{/*https://www.tutorialrepublic.com/twitter-bootstrap-tutorial/bootstrap-list-groups.php*/}
                     <a href="#" className="list-group-item list-group-item-action">
-                        {/*<div className="d-flex w-100 justify-content-between">*/}
-                        <div className="d-flex flex-row-reverse">
-                            {/*<h4>Chair</h4>*/}
-                            13.06.2019
+                        <div className="d-flex justify-content-between text-secondary">
+                                <div><b>Date:</b> 13.06.2019</div>
+                                <div><b>Price:</b> 7,00 €</div>
+                            <div><b>Delivery No.:</b> PA-1906130087</div>
                         </div>
+                        <p/>
                         <Row>
                             <Col className="align-self-center text-center h3">
                                 <p className="h3 font-weight-bold">
@@ -106,35 +43,86 @@ class MonitorList extends Component {
                             </Col>
                             <Col className="align-self-center text-center">
                                 <p>
-                                    <Button variant="secondary">More Info</Button>
+                                    <Button href="#" variant="secondary" size="xs">More Info</Button>
                                 </p>
                                 <p>
-                                    <Button variant="danger">Delete</Button>
+                                    <Button href="#" variant="danger" size="xs">Delete</Button>
                                 </p>
                             </Col>
                         </Row>
-                        {/*<p>Pulvinar leo id risus pellentesque vestibulum. Sed diam libero, sodales eget sapien vel,*/}
-                        {/*    porttitor bibendum enim. Donec sed nibh vitae lorem porttitor blandit in nec ante.</p>*/}
                     </a>
-                    {/*<a href="#" className="list-group-item list-group-item-action active">*/}
-                    {/*    <div className="d-flex w-100 justify-content-between">*/}
-                    {/*        <h4>Scientists found massive black hole</h4>*/}
-                    {/*        <small>2 days ago</small>*/}
-                    {/*    </div>*/}
-                    {/*    <p>Vestibulum id metus ac nisl bibendum scelerisque non non purus. Suspendisse varius nibh non*/}
-                    {/*        aliquet sagittis. In tincidunt orci sit amet elementum vestibulum.</p>*/}
-                    {/*</a>*/}
-                    {/*<a href="#" className="list-group-item list-group-item-action">*/}
-                    {/*    <div className="d-flex w-100 justify-content-between">*/}
-                    {/*        <h4>NASA launches solar probe</h4>*/}
-                    {/*        <small>3 days ago</small>*/}
-                    {/*    </div>*/}
-                    {/*    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at,*/}
-                    {/*        luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante.</p>*/}
-                    {/*</a>*/}
+                    <a href="#" className="list-group-item list-group-item-action">
+                        <div className="d-flex justify-content-between text-secondary">
+                            <div><b>Date:</b> 13.06.2019</div>
+                            <div><b>Price:</b> 20,00 €</div>
+                            <div><b>Delivery No.:</b> PA-1906130084</div>
+                        </div>
+                        <p/>
+                        <Row>
+                            <Col className="align-self-center text-center h3">
+                                <p className="h3 font-weight-bold">
+                                    Dish Set
+                                </p>
+                                <Badge variant="info">In Delivery</Badge>
+                            </Col>
+                            <Col>
+                                <div className="font-weight-bold">Sender</div>
+                                <p>
+                                    Jon Doe<br/>
+                                    Mainstreet 2<br/>
+                                    0000 Musterstadt<br/>
+                                </p>
+                                <div className="font-weight-bold">Recipient</div>
+                                <p>
+                                    Jon Doe<br/>
+                                    Mainstreet 2<br/>
+                                    0000 Musterstadt<br/>
+                                </p>
+                            </Col>
+                            <Col className="align-self-center text-center">
+                                <p>
+                                    <Button variant="secondary">More Info</Button>
+                                </p>
+                            </Col>
+                        </Row>
+                    </a>
+                    <a href="#" className="list-group-item list-group-item-action">
+                        <div className="d-flex justify-content-between text-secondary">
+                            <div><b>Date:</b> 11.06.2019</div>
+                            <div><b>Price:</b> 3,00 €</div>
+                            <div><b>Delivery No.:</b> PA-1906110022</div>
+                        </div>
+                        <p/>
+                        <Row>
+                            <Col className="align-self-center text-center h3">
+                                <p className="h3 font-weight-bold">
+                                    Documents
+                                </p>
+                                <Badge variant="success">Delivered</Badge>
+                            </Col>
+                            <Col>
+                                <div className="font-weight-bold">Sender</div>
+                                <p>
+                                    Jon Doe<br/>
+                                    Mainstreet 2<br/>
+                                    0000 Musterstadt<br/>
+                                </p>
+                                <div className="font-weight-bold">Recipient</div>
+                                <p>
+                                    Jon Doe<br/>
+                                    Mainstreet 2<br/>
+                                    0000 Musterstadt<br/>
+                                </p>
+                            </Col>
+                            <Col className="align-self-center text-center">
+                                <p>
+                                    <Button variant="secondary">More Info</Button>
+                                </p>
+                            </Col>
+                        </Row>
+                    </a>
                 </div>
             </Container>
-
         );
     }
 }
