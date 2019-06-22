058213a830bb9e69cad03b6aea7647265f87fa2f
ALEX-PC\Alexandros <alexandros.tsakpinis@googlemail.com>
2019-06-22 19:41:59 +0200

    Resolved merge mistake

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import NavigationBar from './components/NavigationBar';
-import BiddingOverview from './components/BiddingOverview';
 import React from 'react';
 
 const App = () => {
   return (
       <div className="App">
         <NavigationBar/>
-        <BiddingOverview/>
       </div>
   );
 };
