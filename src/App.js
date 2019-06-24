7662686b10ee640fb2224c67553390f753e54737
MaxiPfeiler <maximilian.pfeiler@tum.de>
2019-06-24 10:30:21 +0200

    Importet CourselHome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Page from './components/Page';
+import Page from './components/Page'
+import CarouselHome from './components/CarouselHome';
 import React from 'react';
 
 const App = () => {
   return (
       <div className="App">
         <Page />
+        <CarouselHome/>
       </div>
   );
 };
