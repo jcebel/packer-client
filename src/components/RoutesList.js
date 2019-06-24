cfd52a1cb843609b726882c3a3b67c43fa83a498
Jonas Ebel <jonas.ebel@tum.de>
2019-06-24 18:59:54 +0200

    Wrap Table in container

--- src/components/RoutesList.js
+++ src/components/RoutesList.js
@@ -6,30 +6,34 @@ import Form from 'react-bootstrap/Form';
 import {RoutesRow} from './RoutesRow';
 import Page from './Page';
 import {EmptyRow} from './EmptyRow'
+import Container from "react-bootstrap/Container";
 
 export class RoutesList extends React.Component {
     // TODO Define Css Stylings!
     render() {
         return (
             <Page>
-                <Table>
-                    <thead>
-                    <tr>
-                        <td><VehicleDropdown/></td>
-                        <td><Form.Control as="input" placeholder="Distance" type="text"/></td>
-                        <td><Form.Control as="input" placeholder="Number of Items" type="text"/></td>
-                        <td><Form.Control as="input" placeholder="Start" type="text"/></td>
-                        <td><Form.Control as="input" placeholder="End" type="text"/></td>
-                        <td><Form.Control as="input" placeholder="Payment" type="text"/></td>
-                        <td><Button variant="danger">Delete Filters</Button></td>
-                    </tr>
-                    </thead>
-                    <tbody>
+                <Container>
+                    <Table>
+                        <thead>
+                        <tr>
+                            <td><VehicleDropdown/></td>
+                            <td><Form.Control as="input" placeholder="Distance" type="text"/></td>
+                            <td><Form.Control as="input" placeholder="Number of Items" type="text"/></td>
+                            <td><Form.Control as="input" placeholder="Start" type="text"/></td>
+                            <td><Form.Control as="input" placeholder="End" type="text"/></td>
+                            <td><Form.Control as="input" placeholder="Payment" type="text"/></td>
+                            <td><Button variant="danger">Delete Filters</Button></td>
+                        </tr>
+                        </thead>
+                        <tbody>
 
-                    {this.props.loadingDone ? this.props.data.map((route, i) => <RoutesRow key={i} route={route}
-                                                                  onMoreInfo={(id) => this.props.onMoreInfo(id)}/>) :  <EmptyRow/> }
-                    </tbody>
-                </Table>
+                        {this.props.loadingDone ? this.props.data.map((route, i) => <RoutesRow key={i} route={route}
+                                                                                               onMoreInfo={(id) => this.props.onMoreInfo(id)}/>) :
+                            <EmptyRow/>}
+                        </tbody>
+                    </Table>
+                </Container>
             </Page>
         );
     }
