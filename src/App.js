86ac96d8ab51d7147af81568aaff822ff5167525
MaxiPfeiler <maximilian.pfeiler@tum.de>
2019-06-24 09:45:31 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
+import Page from './components/Page';
+import React from 'react';
 
-
-
-/*const App = () => {
+const App = () => {
   return (
       <div className="App">
         <Page />
       </div>
   );
-};*/
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <NavigationBar/>
-
-        </div>
-
-      );
-   };
 };
-
-
-
-
 
 export default App;
