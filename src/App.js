1eea34e9d444777a0a2d850395f4cc930f3482e6
MaxiPfeiler <maximilian.pfeiler@tum.de>
2019-06-24 14:16:34 +0200

    Use Page instead of Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom";
-import {NavigationBar} from './components/NavigationBar';
+import Page from './components/Page';
 import Home from './components/Home';
 
 function App() {
     return (
         <Router>
-            <NavigationBar/>
+            <Page/>
             <div>
                 {
                     //TODO : This needs to be routed to the home component @MaxiPfeiler
