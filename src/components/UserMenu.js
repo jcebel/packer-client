e1c86845dbf7165508f8083f950fd7ad49bb7037
Lukas <lukas.vorwerk@tum.de>
2019-07-03 16:46:17 +0200

    add register button to UserMenu

--- src/components/UserMenu.js
+++ src/components/UserMenu.js
@@ -41,7 +41,9 @@ class UserMenu extends React.Component {
                          }>
                 {this.state.user ?[
                     <NavDropdown.Item key={1} onClick={() => this.logout()}>Logout</NavDropdown.Item>
-            ]: [<NavDropdown.Item key={1} onClick={()=> this.props.history.push('/login')}>Login</NavDropdown.Item>]}
+            ]: [<NavDropdown.Item key={1} onClick={()=> this.props.history.push('/login')}>Login</NavDropdown.Item>,
+                <NavDropdown.Item key={2} onClick={()=> this.props.history.push('/register')}>Register</NavDropdown.Item>
+                ]}
 
             </NavDropdown>
         )
