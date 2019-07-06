755e818a59f006a42ee115ebc9127e2cb2f19ac9
ALEX-PC\Alexandros <alexandros.tsakpinis@googlemail.com>
2019-07-06 14:53:32 +0200

    Changed styled compoenents

--- src/views/BiddingProcessView.js
+++ src/views/BiddingProcessView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import {Container, Row, Col, Card} from 'react-bootstrap';
 import GoogleMaps from '../components/GoogleMaps'
 import {RouteDetails} from "../components/RouteDetails";
 import {PackageList} from "../components/PackageList";
@@ -7,9 +7,10 @@ import {BiddingInformation} from "../components/BiddingInformation";
 import {RouteService} from "../services/RouteService";
 import Page from "../components/Page";
 import UserService from "../services/UserService";
-import {StyledCard} from "../components/StyledCard";
-import {StyledRow} from "../components/StyledRow";
+import styled from 'styled-components/macro';
 
+const StyledRow = styled(Row)`height:"350px"`;
+const StyledCard = styled(Card)`height:"350px"`;
 
 export class BiddingProcessView extends React.Component{
 
