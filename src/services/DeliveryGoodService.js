27073a362df1d0c006cad459142e5a212104ca4c
Lukas <lukas.vorwerk@tum.de>
2019-07-12 13:50:10 +0200

    add connection to database with static id

--- src/services/DeliveryGoodService.js
+++ src/services/DeliveryGoodService.js
@@ -4,6 +4,21 @@ export class DeliveryGoodService{
 
     static baseURL() {return "http://localhost:3000/deliverygoods" }
 
+    static getDeliveryGood(id) {
+        return new Promise((resolve, reject) => {
+            HttpService.get(`${this.baseURL()}/${id}`, function(data) {
+                if(data !== undefined || Object.keys(data).length !== 0) {
+                    resolve(data);
+                }
+                else {
+                    reject('Error while retrieving Delivery Good');
+                }
+            }, function(textStatus) {
+                reject(textStatus);
+            });
+        });
+    }
+
     static deleteDeliveryGood(id) {
         return new Promise((resolve, reject) => {
             HttpService.remove(`${this.baseURL()}/${id}`, function(data) {
--- src/views/DeliveryDetailsView.js
+++ src/views/DeliveryDetailsView.js
@@ -1,24 +1,51 @@
 import React from 'react';
+import {DeliveryGoodService} from "../services/DeliveryGoodService";
 
 export class DeliveryDetailsView extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            loadingDone: true,
-            data: []
-        }
+            loading: false,
+            delGood: {}
+        };
     };
 
     componentWillMount() {
         this.setState({
             loadingDone: false
         });
+        this.refreshDelGoodData();
+    }
+
+    refreshDelGoodData(){
+        let id = this.props.match.params.id;
+        id = "5d286e9fae02f13a9893801b";
+        DeliveryGoodService.getDeliveryGood(id).then((data) => {
+            this.setState({
+                delGood: data,
+                loading: false
+            });
+        }).catch((e) => {
+            console.error(e);
+        });
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => this.refreshDelGoodData(), 5000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
     }
 
     render() {
+        if (this.state.loading) {
+            return (<h2>Loading...</h2>);
+        }
+
         return (
-            <div>DeliveryDetailsView</div>
+            <div>{JSON.stringify(this.state.delGood)}</div>
         );
     }
 }
