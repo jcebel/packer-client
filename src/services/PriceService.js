625ee558ba3539add438892ee1562717d292c1ae
MaxiPfeiler <maximilian.pfeiler@tum.de>
2019-07-14 17:26:13 +0200

    changes from Alex

--- src/services/PriceService.js
+++ src/services/PriceService.js
@@ -2,13 +2,13 @@ import {HttpService} from './HttpService';
 
 export class PriceService {
 
-    static baseURL() {return "http://localhost:3000/price" }
+    static baseURL() {return HttpService.apiURL() + "/price"; }
     //static baseURL() {return "http://ec2-3-16-129-205.us-east-2.compute.amazonaws.com:3000/route" }
 
 
     static createPriceCalculation(parameters) {
         return new Promise((resolve, reject) => {
-            HttpService.post(`${this.baseURL()}/`, parameters, function(data) {
+            HttpService.post(`${this.baseURL()}`, parameters, function(data) {
                 resolve(data);
             }, function(textStatus) {
                 reject(textStatus);
