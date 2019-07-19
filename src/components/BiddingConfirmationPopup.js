cba259b038ddf80fe9b05b72207ec34150c63cbd
Lukas <lukas.vorwerk@tum.de>
2019-07-19 13:40:41 +0200

    fix popup after unsuccessful submit

--- src/components/BiddingConfirmationPopup.js
+++ src/components/BiddingConfirmationPopup.js
@@ -3,10 +3,18 @@ import Popup from "reactjs-popup";
 
 export class BiddingConfirmationPopup extends React.Component {
 
+    isEmpty(obj) {
+        for(let key in obj) {
+            if(obj.hasOwnProperty(key))
+                return false;
+        }
+        return true;
+    }
+
     render() {
         return (
             <Popup trigger={<button type="submit" className="btn btn-primary mr-2">Submit</button>}
-            position={"top center"}
+            position={"top center"} disabled={!this.isEmpty(this.props.error)}
             >
                 <div><b>Submitted successfully!</b></div>
             </Popup>
